ef65af571e0c654be69a584eef80b9d1e3a07188
Rudi Batubara <rudi.batubara@live.com>
2022-11-23 22:03:05 +0700

    Add createFastContext util to main.tsx
    We want to create a initial data from main.tsx and child compenent just need to use `useStore` custom hook.

--- fast-context-generic/src/main.tsx
+++ fast-context-generic/src/main.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import createFastContext from './src/createFastContext'
 import App from './App'
 import './index.css'
 
+interface CategoryType {
+  alt: string;
+  src: string;
+  title: string;
+}
+
+interface ProductType extends CategoryType {
+  price: string | number;
+  discount: number;
+  stock: number;
+}
+
+declare global {
+  interface Window {
+    categories: CategoryType[];
+    products: ProductType[];
+  }
+}
+
+const { Provider, useStore } = createFastContext({
+  categories: Object.assign({}, (typeof window !== 'undefined') ? window.categories : {});,
+  products: Object.assign({}, (typeof window !== 'undefined') ? window.products : {});,
+});
+
+/*
+ * How to passing `useStore` to App.tsx
+ * without re-declare createFastContext() in the App.tsx or to
+ * deep child components
+ */ 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
